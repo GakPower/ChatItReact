5e8cb98b2f0ad773fa9dc92d6180660d5d346d62
GakPower <alanigogos@gmail.com>
2020-10-05 20:48:39 +0200

    added script for server manipulation

---
+++ src/ServerUtils.ts
@@ -0,0 +1,17 @@
+export const registerUser = ({
+	username,
+	email,
+	password,
+}: {
+	username: string;
+	email: string;
+	password: string;
+}) => {
+	return fetch('localhost:5000/register', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ username, email, password }),
+	});
+};
