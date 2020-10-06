57e13c52ecd027e06a89f173540fce59c580c18a
GakPower <alanigogos@gmail.com>
2020-10-06 23:51:46 +0200

    added login request

--- src/ServerUtils.ts
+++ src/ServerUtils.ts
@@ -7,11 +7,29 @@ export const registerUser = ({
 	email: string;
 	password: string;
 }) => {
-	return fetch('http://localhost:5000/register', {
+	return fetch('http://localhost:5000/join', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
 		},
 		body: JSON.stringify({ username, email, password }),
+	});
+};
+
+export const loginUser = (
+	{
+		username,
+		email,
+		password,
+	}: { username?: string; email?: string; password: string },
+	usernameLogin: boolean
+) => {
+	const fieldToPass = usernameLogin ? { username } : { email };
+	return fetch('http://localhost:5000/login', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ fieldToPass, password }),
 	});
 };
