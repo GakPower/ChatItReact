79487bb7b3c4cf7cd966c3fd1778f733c2a01629
GakPower <alanigogos@gmail.com>
2020-10-16 21:27:38 +0200

    added footer

--- src/components/secondary/Footer/Footer.tsx
+++ src/components/secondary/Footer/Footer.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Email } from '../../../assets/icons/Email';
+import { GitHub } from '../../../assets/icons/Github';
+import { LinkedIn } from '../../../assets/icons/LinkedIn';
 import './Footer.scss';
 export const Footer = () => {
 	return (
 		<div id='footer'>
+			<div id='contacts'>
+				<Email link='mailto:gkotDev@gmail.com' />
+				<GitHub link='https://github.com/GakPower' />
+				<LinkedIn link='https://www.linkedin.com/in/george-kotsiopoulos-a2b0971a4/' />
+			</div>
+
+			<div id='separator'></div>
+
 			<Link to='/privacyPolicy'>Privacy Policy</Link>
 		</div>
 	);
