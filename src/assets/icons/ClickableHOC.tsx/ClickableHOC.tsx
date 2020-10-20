1662258a48207fbe639695add7dffdfe6b8c53ff
GakPower <alanigogos@gmail.com>
2020-10-20 22:18:39 +0200

    minor changes

--- src/assets/icons/ClickableHOC.tsx/ClickableHOC.tsx
+++ src/assets/icons/ClickableHOC.tsx/ClickableHOC.tsx
@@ -2,16 +2,25 @@ import React from 'react';
 import './Style.scss';
 
 export const ClickableIcon = (Icon: any) => {
-	const clickable = ({ link, ...rest }: { link: string }) => {
+	const clickable = ({
+		link,
+		onCLick,
+		fill,
+	}: {
+		link?: string;
+		onCLick?: any;
+		fill?: string;
+	}) => {
 		return (
 			<a
 				style={{ display: 'flex', margin: '0 3px' }}
 				href={link}
 				target='_blank'
 				rel='noopener noreferrer'
-				className='icon'
+				className={link || onCLick ? 'icon' : ''}
+				onClick={onCLick}
 			>
-				<Icon {...rest} />
+				<Icon fill={fill} />
 			</a>
 		);
 	};
