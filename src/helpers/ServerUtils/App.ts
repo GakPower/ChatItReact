4c140095b7fd6d66e601b889d296ddaa3a9c4fd4
GakPower <alanigogos@gmail.com>
2020-10-23 18:28:02 +0200

    separated ServerUtils to App and Auth

---
+++ src/helpers/ServerUtils/App.ts
@@ -0,0 +1,16 @@
+const SERVER_IP = 'http://localhost:5000';
+const AUTH_PATH = `${SERVER_IP}/api/app`;
+export const getEmojis = async () => {
+	try {
+		const res = await fetch(`${AUTH_PATH}/emojis`, {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		const jsonRes = await res.json();
+		return jsonRes.emojis;
+	} catch (error) {
+		console.log(error);
+		return [];
+	}
+};
